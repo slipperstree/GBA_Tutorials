6052f2a1d58ce4c0801297a83e0db743e34bc48f
JamieDStewart <stewart.jamie.d@gmail.com>
2019-02-27 17:18:45 +0000

    added comments so that the internet doesn't think I'm some kind of scum

--- 000_Hello_Pixel/source/main.c
+++ 000_Hello_Pixel/source/main.c
@@ -1,23 +1,38 @@
+//\===========================================================================================================
+//\ Title: Main.c 
+//\ Author: Jamie Stewart
+//\ About: 
+//\        This is a simple Pixel Plotter for the GBA that does not feature any user input and draws 
+//\   it's content to the screenbuffer using bitmap mode rendering.
+//\===========================================================================================================
+
 #include "Intellisense.h"
-
-
 #include <stdint.h>
 #include <stdbool.h>
 
+//\===========================================================================================================
+//\TYPEDEF our variables to indicate clearly what level of bit precision each variable has
+//\===========================================================================================================
 typedef uint8_t		uint8;
 typedef uint16_t	uint16;
 typedef uint32_t	uint32;
 typedef int8_t		int8;
 typedef int16_t		int16;
 typedef int32_t		int32;
-
+//\===========================================================================================================
+//\ Volatile variables
+//\ The volatile key word indicates to the compiler, and the programmer. That these variables may have their values
+//\ altered by an external factor, this could be a hardware switch, or some external program that can access the 
+//\ memory location of the variable.
+//\===========================================================================================================
 typedef volatile uint8_t		v_uint8;
 typedef volatile uint16_t		v_uint16;
 typedef volatile uint32_t		v_uint32;
 typedef volatile int8_t			v_int8;
 typedef volatile int16_t		v_int16;
 typedef volatile int32_t		v_int32;
-
+//\===========================================================================================================
+//\ Set up defines for making sense of some memory addresses
 #define REG_DISPLAYCONTROL *((v_uint32*)(0x04000000))
 #define VIDEOMODE_3 0x0003
 #define BGMODE_2	0x0400
@@ -25,20 +40,28 @@ typedef volatile int32_t		v_int32;
 #define SCREENBUFFER ((v_int16*)(0x06000000))
 #define SCREEN_W 240
 #define SCREEN_H 160
-
-#define REG_VCOUNT (*(v_uint16*)(0x04000006))
-
+//\===========================================================================================================
+//\ Function to get the absolute value of a variable passed into it. This function uses bitwise operators
+//\ The value sent in is bitshifted right 31 bits, this will leave only the most significant bit the sign bit.
+//\ if the sign bit is on then this is a negative integer value, shifting it 31 spaces right leaves us with a 
+//\ value of 0 or all 1's for a -tve number (this is really implementation independent, but works on the GBA arm 
+//\ implementation). We then XOR this back into the original value which will remove the negative (this is two's 
+//\ compliment at work) and give us a positive value if the mask was negative we add one (mask is either 0 or -1)
+//\ and get our absolute number, no if statments involved. GBA code has to be quick.
 int32 abs(int32 a_val)
 {
 	int32 mask = a_val >> 31;
 	return (a_val ^ mask) - mask;
 }
-
+//\===========================================================================================================
+//\ Function takes three 8 bit values and uses these to set a 16 bit value, the & 0x1F ensures that the values are
+//\ all less than 5 bits in size, any number higher than 31 will be truncated.
 uint16 setColor(uint8 a_red, uint8 a_green, uint8 a_blue)
 {
 	return (a_red & 0x1F) | (a_green & 0x1F) << 5 | (a_blue & 0x1f) << 10;
 }
-
+//\===========================================================================================================
+//\function to fill a rectangular area of the screen with the colour value provided to it as the 5th argument.
 void drawRect(int32 a_left, int32 a_top, int32 a_width, int32 a_height, uint16 a_color)
 {
 	for (uint32 y = 0; y < a_height; ++y)
@@ -49,7 +72,9 @@ void drawRect(int32 a_left, int32 a_top, int32 a_width, int32 a_height, uint16 a
 		}
 	}
 }
-
+//\===========================================================================================================
+//\ an implementation of bresenham's line drawing algorithm, allows lines to be drawn in any direction
+//\ from positive to negative direction, vertically or horizontally
 void drawLine(int32 a_x, int32 a_y, int32 a_x2, int32 a_y2, uint16 a_color)
 {
 	int32 w = a_x2 - a_x;
@@ -85,7 +110,7 @@ void drawLine(int32 a_x, int32 a_y, int32 a_x2, int32 a_y2, uint16 a_color)
 
 int main()
 {
-	//set GBA rendering context to MODE 3 Bitmap Rendering
+	//set GBA rendering context to MODE 3 Bitmap Rendering and enable BG 2
 	REG_DISPLAYCONTROL = VIDEOMODE_3 | BGMODE_2;
 
 
