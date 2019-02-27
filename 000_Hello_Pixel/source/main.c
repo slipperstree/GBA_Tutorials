3660786e5b1c1248d455eefcbd667e86a72484c1
TeaCoder <stewart.jamie.d@gmail.com>
2019-02-27 21:01:37 +0000

    slight tidy up of bresenhams line drawing with comments

--- 000_Hello_Pixel/source/main.c
+++ 000_Hello_Pixel/source/main.c
@@ -61,6 +61,12 @@ uint16 setColor(uint8 a_red, uint8 a_green, uint8 a_blue)
 	return (a_red & 0x1F) | (a_green & 0x1F) << 5 | (a_blue & 0x1f) << 10;
 }
 //\===========================================================================================================
+void plotPixel( int32 a_x, int32 a_y, uint16 a_colour)
+{
+	SCREENBUFFER[a_y * SCREEN_W + a_x] = a_colour;
+}
+
+//\===========================================================================================================
 //\function to fill a rectangular area of the screen with the colour value provided to it as the 5th argument.
 void drawRect(int32 a_left, int32 a_top, int32 a_width, int32 a_height, uint16 a_color)
 {
@@ -68,39 +74,55 @@ void drawRect(int32 a_left, int32 a_top, int32 a_width, int32 a_height, uint16 a
 	{
 		for (uint32 x = 0; x < a_width; ++x)
 		{
-			SCREENBUFFER[(a_top + y) * SCREEN_W + a_left + x] = a_color;
+			plotPixel(a_left + x, a_top + y, a_color);
 		}
 	}
 }
 //\===========================================================================================================
 //\ an implementation of bresenham's line drawing algorithm, allows lines to be drawn in any direction
 //\ from positive to negative direction, vertically or horizontally
-void drawLine(int32 a_x, int32 a_y, int32 a_x2, int32 a_y2, uint16 a_color)
+void drawLine(int32 a_x, int32 a_y, int32 a_x2, int32 a_y2, uint16 a_colour)
 {
-	int32 w = a_x2 - a_x;
-	int32 h = a_y2 - a_y;
+	//Get the horizontal and vertical displacement of the line
+	int32 w = a_x2 - a_x; //w is width or horizontal distance
+	int32 h = a_y2 - a_y; //h is the height or vertical displacement
+	//work out what the change in x and y is with the d in these variables stands for delta
 	int32 dx1 = 0, dy1 = 0, dx2 = 0, dy2 = 0;
-	if (w<0) dx1 = -1; else if (w>0) dx1 = 1;
+
+	if (w<0) dx1 = dx2 = -1; else if (w>0) dx1 = dx2 = 1;
 	if (h<0) dy1 = -1; else if (h>0) dy1 = 1;
-	if (w<0) dx2 = -1; else if (w>0) dx2 = 1;
-	int32 longest = abs(w);
+	//which is the longest the horizontal or vertical step
+	int32 longest = abs(w); //assume that width is the longest displacement
 	int32 shortest = abs(h);
-	if (!(longest>shortest)) {
-		longest = abs(h);
-		shortest = abs(w);
+	if ( shortest > longest )	//oops it's the other way around reverse it
+	{
+		//use xor to swap longest and shortest around
+		longest ^= shortest; shortest ^= longest; longest ^= shortest;
 		if (h<0) dy2 = -1; else if (h>0) dy2 = 1;
 		dx2 = 0;
 	}
+	//geta  value that is half the longest displacement
 	int32 numerator = longest >> 1;
-	for (int32 i = 0; i <= longest; i++) {
-		SCREENBUFFER[a_y * SCREEN_W + a_x] = a_color;
+	//for each pixel across the longest span
+	for (int32 i = 0; i <= longest; ++i)
+	{
+		//fill the pixel we're currently at
+		plotPixel( a_x, a_y, a_colour);
+		//increase the numerator by the shortest span
 		numerator += shortest;
-		if (!(numerator<longest)) {
+		if (numerator>longest)  
+		{
+			//if the numerator is now larger than the longest span
+			//subtract the longest value from the numerator
 			numerator -= longest;
+			//increment x & y by their delta1 values
 			a_x += dx1;
 			a_y += dy1;
 		}
-		else {
+		else 
+		{
+			//numerator is smaller than the longst side
+			//increment x & y by their delta 2 values
 			a_x += dx2;
 			a_y += dy2;
 		}
@@ -120,7 +142,7 @@ int main()
 
 		drawLine(10, 4, 230, 156, setColor(2, 31, 15));
 		drawLine(10, 156, 230, 4, setColor(2, 15, 31));
-
+		
 		drawRect(100, 60, 40, 40, setColor(31,12,3));
 	}
 	
