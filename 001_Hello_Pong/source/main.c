6c6dabbe7bf2f084c8e5e9974d1d276eed29010d
JamieDStewart <stewart.jamie.d@gmail.com>
2019-03-01 09:46:45 +0000

    minor shifts in layout

--- 001_Hello_Pong/source/main.c
+++ 001_Hello_Pong/source/main.c
@@ -16,27 +16,21 @@
 //\===========================================================================================================
 //\TYPEDEF our variables to indicate clearly what level of bit precision each variable has
 //\===========================================================================================================
-typedef uint8_t		u8;
-typedef uint16_t	u16;
-typedef uint32_t	u32;
-typedef int8_t		s8;
-typedef int16_t		s16;
-typedef int32_t		s32;
+typedef uint8_t		u8;		typedef int8_t		s8;
+typedef uint16_t	u16;	typedef int16_t		s16;
+typedef uint32_t	u32;	typedef int32_t		s32;
 //\===========================================================================================================
 //\ Volatile variables
 //\ The volatile key word indicates to the compiler, and the programmer. That these variables may have their values
 //\ altered by an external factor, this could be a hardware switch, or some external program that can access the 
 //\ memory location of the variable.
 //\===========================================================================================================
-typedef volatile uint8_t		v_u8;
-typedef volatile uint16_t		v_u16;
-typedef volatile uint32_t		v_u32;
-typedef volatile int8_t			v_s8;
-typedef volatile int16_t		v_s16;
-typedef volatile int32_t		v_s32;
+typedef volatile uint8_t		v_u8;	typedef volatile int8_t			v_s8;
+typedef volatile uint16_t		v_u16;	typedef volatile int16_t		v_s16;
+typedef volatile uint32_t		v_u32;	typedef volatile int32_t		v_s32;
 //\===========================================================================================================
 //\ Set up defines for making sense of some memory addresses
-#define REG_DISPCNT *((v_u16*)(0x04000000))
+#define REG_DISPCNT *((v_u32*)(0x04000000))
 #define VIDEOMODE_3 0x0003
 #define BGMODE_2	0x0400
 
@@ -63,52 +57,69 @@ u16 setColor(u8 a_red, u8 a_green, u8 a_blue)
 {
 	return (a_red & 0x1F) | (a_green & 0x1F) << 5 | (a_blue & 0x1f) << 10;
 }
-
-void plotPixel( s32 a_x, s32 a_y, u16 a_colour)
+//\===========================================================================================================
+void plotPixel( u32 a_x, u32 a_y, u16 a_colour)
 {
 	SCREENBUFFER[a_y * SCREEN_W + a_x] = a_colour;
 }
+
 //\===========================================================================================================
 //\function to fill a rectangular area of the screen with the colour value provided to it as the 5th argument.
-void drawRect(s32 a_left, s32 a_top, s32 a_width, s32 a_height, u16 a_color)
+void drawRect(u32 a_left, u32 a_top, u32 a_width, u32 a_height, u16 a_color)
 {
 	for (u32 y = 0; y < a_height; ++y)
 	{
 		for (u32 x = 0; x < a_width; ++x)
 		{
-			SCREENBUFFER[(a_top + y) * SCREEN_W + a_left + x] = a_color;
+			SCREENBUFFER[ (a_top + y) * SCREEN_W + a_left + x ] = a_color;
 		}
 	}
 }
 //\===========================================================================================================
 //\ an implementation of bresenham's line drawing algorithm, allows lines to be drawn in any direction
 //\ from positive to negative direction, vertically or horizontally
-void drawLine(s32 a_x, s32 a_y, s32 a_x2, s32 a_y2, u16 a_color)
+void drawLine(u32 a_x, u32 a_y, u32 a_x2, u32 a_y2, u16 a_colour)
 {
-	s32 w = a_x2 - a_x;
-	s32 h = a_y2 - a_y;
+	//Get the horizontal and vertical displacement of the line
+	s32 w = a_x2 - a_x; //w is width or horizontal distance
+	s32 h = a_y2 - a_y; //h is the height or vertical displacement
+	//work out what the change in x and y is with the d in these variables stands for delta
 	s32 dx1 = 0, dy1 = 0, dx2 = 0, dy2 = 0;
-	if (w<0) dx1 = -1; else if (w>0) dx1 = 1;
+
+	if (w<0) dx1 = dx2 = -1; else if (w>0) dx1 = dx2 = 1;
 	if (h<0) dy1 = -1; else if (h>0) dy1 = 1;
-	if (w<0) dx2 = -1; else if (w>0) dx2 = 1;
-	s32 longest = abs(w);
+	//which is the longest the horizontal or vertical step
+	s32 longest = abs(w); //assume that width is the longest displacement
 	s32 shortest = abs(h);
-	if (!(longest>shortest)) {
-		longest = abs(h);
-		shortest = abs(w);
+	if ( shortest > longest )	//oops it's the other way around reverse it
+	{
+		//use xor to swap longest and shortest around
+		longest ^= shortest; shortest ^= longest; longest ^= shortest;
 		if (h<0) dy2 = -1; else if (h>0) dy2 = 1;
 		dx2 = 0;
 	}
+	//geta  value that is half the longest displacement
 	s32 numerator = longest >> 1;
-	for (u32 i = 0; i <= longest; i++) {
-		SCREENBUFFER[a_y * SCREEN_W + a_x] = a_color;
+	//for each pixel across the longest span
+	for (s32 i = 0; i <= longest; ++i)
+	{
+		//fill the pixel we're currently at
+		plotPixel( a_x, a_y, a_colour);
+		//increase the numerator by the shortest span
 		numerator += shortest;
-		if (!(numerator<longest)) {
+		if (numerator>longest)  
+		{
+			//if the numerator is now larger than the longest span
+			//subtract the longest value from the numerator
 			numerator -= longest;
+			//increment x & y by their delta1 values
 			a_x += dx1;
 			a_y += dy1;
 		}
-		else {
+		else 
+		{
+			//numerator is smaller than the longst side
+			//increment x & y by their delta 2 values
 			a_x += dx2;
 			a_y += dy2;
 		}
@@ -208,7 +219,8 @@ void MoveBall(Ball* a_ball)
 
 	if (reset)
 	{
-		a_ball->x = SCREEN_W/2 - a_ball->size/2; a_ball->y = SCREEN_H/2 - a_ball->size/2;
+		a_ball->x = SCREEN_W/2 - a_ball->size/2; 
+		a_ball->y = SCREEN_H/2 - a_ball->size/2;
 		a_ball->xDir = 0;
 		StartBall(a_ball);
 	}
